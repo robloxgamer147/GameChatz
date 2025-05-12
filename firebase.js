ad05474d65db48ad5590aef82b87034df6359493
robloxgamer147 <robloxgamer1472@gmail.com>
2025-05-12 21:33:22 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,14 @@
-// Firebase SDKs
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
-import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
 
-// Initialize Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyBuLyGwCrPY26ohmHY4YwLVwF0dlAfkzak",
   authDomain: "gamechatz-e61e7.firebaseapp.com",
   databaseURL: "https://gamechatz-e61e7-default-rtdb.firebaseio.com",
   projectId: "gamechatz-e61e7",
-  storageBucket: "gamechatz-e61e7.firebasestorage.app",
+  storageBucket: "gamechatz-e61e7.appspot.com",
   messagingSenderId: "155399717230",
   appId: "1:155399717230:web:ae414fa8739baf63d0c17f",
   measurementId: "G-5XW4J2BQ7N"
@@ -16,3 +16,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getDatabase(app);
+
+export { auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, db, ref, set };
