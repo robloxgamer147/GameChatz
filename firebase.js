05312d7b6d8691aa26aeef2d240b721c93fa9225
robloxgamer147 <robloxgamer1472@gmail.com>
2025-05-12 17:00:17 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,17 +1,13 @@
-// Import Firebase configuration and initialize the app
-import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
-
+// Initialize Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyBuLyGwCrPY26ohmHY4YwLVwF0dlAfkzak",
-    authDomain: "gamechatz-e61e7.firebaseapp.com",
-    databaseURL: "https://gamechatz-e61e7-default-rtdb.firebaseio.com",
-    projectId: "gamechatz-e61e7",
-    storageBucket: "gamechatz-e61e7.firebasestorage.app",
-    messagingSenderId: "155399717230",
-    appId: "1:155399717230:web:ae414fa8739baf63d0c17f",
-    measurementId: "G-5XW4J2BQ7N"
+  apiKey: "AIzaSyBuLyGwCrPY26ohmHY4YwLVwF0dlAfkzak",
+  authDomain: "gamechatz-e61e7.firebaseapp.com",
+  databaseURL: "https://gamechatz-e61e7-default-rtdb.firebaseio.com",
+  projectId: "gamechatz-e61e7",
+  storageBucket: "gamechatz-e61e7.firebasestorage.app",
+  messagingSenderId: "155399717230",
+  appId: "1:155399717230:web:ae414fa8739baf63d0c17f",
+  measurementId: "G-5XW4J2BQ7N"
 };
 
-const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
+const app = firebase.initializeApp(firebaseConfig);
